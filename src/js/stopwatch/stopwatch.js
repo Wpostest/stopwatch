d45a49cdf18eca66f7c83b830ca98984b233a738
Filip Kwiatkowski <filip.kwiatkowski3@wp.pl>
2019-08-06 14:57:15 +0200

    Stoper ukonczony dodano funckje do wypisywania rund i zerowania stoperu, zmienna stopwatchTime zostala przeniesiona i zdeklarowana w objekcie, dodano nowa zmienna stopwatchStartStopFlag do objektu 2019.08.06

--- src/js/stopwatch/stopwatch.js
+++ src/js/stopwatch/stopwatch.js
@@ -3,21 +3,24 @@ const stopwatchBtnRound = document.querySelector('.stopwatch__btn--round');
 const stopwatchMinutes = document.querySelector('.stopwatch__minutes');
 const stopwatchSeconds = document.querySelector('.stopwatch__seconds');
 const stopwatchMiniseconds = document.querySelector('.stopwatch__miniseconds');
+const stopwatchDisplayRound = document.querySelector('.stopwatch__display-round');
 
 const stopwatchValues = {
     indexStopwatchInterval: 0,
     stopwatchTimeSaved: 0,
+    stopwatchStartStopFlag: 0,
+    stopwatchTime: 0,
 }
 
 const startStopwatch = function(event){
     let clickFlag = 0;
-    let stopwatchTime = 0;
     stopwatchValues.stopwatchTimeSaved = 0;
     
     return function(event){
         switch(clickFlag){
             case 0:{
                 clickFlag = 1;
+                stopwatchValues.stopwatchStartStopFlag = 1;
                 const curremtTimeWhenClickStartStopwatch = new Date().getTime();
                 stopwatchBtnStart.classList.add('h-change-border-color');
                 stopwatchBtnRound.classList.add('h-change-backgorund-color');
@@ -28,26 +31,28 @@ const startStopwatch = function(event){
                     const currentTimeAfterStartStopWatch = new Date().getTime();
                     
                     if(stopwatchValues.stopwatchTimeSaved == 0){
-                        stopwatchTime = currentTimeAfterStartStopWatch - curremtTimeWhenClickStartStopwatch;
+                        stopwatchValues.stopwatchTime = currentTimeAfterStartStopWatch - curremtTimeWhenClickStartStopwatch;
                     }
                     else{
-                        stopwatchTime = currentTimeAfterStartStopWatch - curremtTimeWhenClickStartStopwatch + stopwatchValues.stopwatchTimeSaved;
+                        stopwatchValues.stopwatchTime = currentTimeAfterStartStopWatch - curremtTimeWhenClickStartStopwatch + stopwatchValues.stopwatchTimeSaved;
                     }
 
-                    stopwatchMinutes.textContent = Math.floor(stopwatchTime / 1000 / 60 % 60) < 10 ? "0" + Math.floor(stopwatchTime / 1000 / 60 % 60) : Math.floor(stopwatchTime / 1000 / 60 & 60);
-                    stopwatchSeconds.textContent = Math.floor(stopwatchTime / 1000 % 60) < 10 ? "0" + Math.floor(stopwatchTime / 1000 % 60) : Math.floor(stopwatchTime / 1000 % 60);
-                    stopwatchMiniseconds.textContent = (stopwatchTime % 1000).toString().substring(0, 2);
+                    stopwatchMinutes.textContent = Math.floor(stopwatchValues.stopwatchTime / 1000 / 60) < 10 ? "0" + Math.floor(stopwatchValues.stopwatchTime / 1000 / 60) : Math.floor(stopwatchValues.stopwatchTime / 1000 / 60);
+                    stopwatchSeconds.textContent = Math.floor(stopwatchValues.stopwatchTime / 1000 % 60) < 10 ? "0" + Math.floor(stopwatchValues.stopwatchTime / 1000 % 60) : Math.floor(stopwatchValues.stopwatchTime / 1000 % 60);
+                    stopwatchMiniseconds.textContent = (stopwatchValues.stopwatchTime % 1000).toString().substring(0, 2);
                 }, 90);
 
                 break;
             }
             case 1:{
                 clickFlag = 0;
+                stopwatchValues.stopwatchStartStopFlag = 0;
+                stopwatchValues.stopwatchStartStop = 0;
                 stopwatchBtnStart.classList.remove('h-change-border-color');
                 stopwatchBtnRound.textContent = "Wyzeruj";
                 stopwatchBtnStart.textContent = "Start";
                 clearInterval(stopwatchValues.indexStopwatchInterval);
-                stopwatchValues.stopwatchTimeSaved = stopwatchTime; 
+                stopwatchValues.stopwatchTimeSaved = stopwatchValues.stopwatchTime; 
 
                 break;
             }
@@ -55,4 +60,36 @@ const startStopwatch = function(event){
     }
 };
 
+const resetAndRoundStopwatch = function(){
+    switch(stopwatchValues.stopwatchStartStopFlag){
+        case 0:{
+            clearInterval(stopwatchValues.indexStopwatchInterval);
+            stopwatchDisplayRound.textContent = "";
+            stopwatchValues.stopwatchValues = 0;
+            stopwatchValues.stopwatchTimeSaved = 0;
+            stopwatchValues.stopwatchStartStopFlag = 0;
+            stopwatchValues.stopwatchTime = 0;
+            stopwatchMinutes.textContent = "00";
+            stopwatchSeconds.textContent = "00";
+            stopwatchMiniseconds.textContent = "00";
+
+            break;
+        }
+        case 1:{
+            const p = document.createElement('p');
+            p.textContent += stopwatchMinutes.textContent + ":";
+            p.textContent += stopwatchSeconds.textContent + ",";
+            p.textContent += stopwatchMiniseconds.textContent;
+
+            const span = document.createElement('span');
+            span.textContent = "Runda " + document.querySelectorAll('.stopwatch__display-round p').length;
+
+            stopwatchDisplayRound.appendChild(span);
+            stopwatchDisplayRound.appendChild(p);
+            break;
+        }
+    }
+}
+
 stopwatchBtnStart.addEventListener('click', startStopwatch());
+stopwatchBtnRound.addEventListener('click', resetAndRoundStopwatch);
